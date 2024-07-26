1609d49dc315bbe4c72d9740455446ce8a50532b
Matias <mati.dehoyosmdp@gmail.com>
2024-07-26 20:37:39 -0300

    header

--- cliente/src/components/Header.jsx
+++ cliente/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
   return (
-        <div class="bg-[url('/bg1.jpeg')] bg-bottom bg-cover min-h-screen w-screen pt-20">
+        <div class="bg-[url('/bg1.jpeg')] bg-bottom bg-cover h-screen w-screen pt-20">
                 <p className='font-normal tracking-[.03em] text-white opacity-60 text-center text-lg drop-shadow-md w-60 m-auto leading-5'>¿Buscas un sitio web que represente a tu negocio?</p>
                 <img src="./marca.png" alt="Logo Modo Digital." className='w-60 mx-auto mt-16' />
                 <h1 className='w-96 mt-1 mx-auto text-center text-white opacity-85 font-montserrat font-bold text-5xl tracking-tighter leading-10 drop-shadow-md'>Diseño web <br/>profesional</h1>
