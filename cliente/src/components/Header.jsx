3b845ba696ff5fd421ac0703869f1002d4d23ba3
Matias <mati.dehoyosmdp@gmail.com>
2024-07-25 21:49:52 -0300

    navbar header

--- cliente/src/components/Header.jsx
+++ cliente/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
   return (
-<div class="bg-[url('./bg.png')] bg-cover h-screen w-screen pt-12">
+<div class="bg-[url('./bg.png')] bg-bottom bg-cover min-h-screen w-screen pt-20">
     <p className='font-semibold tracking-[.03em] text-center text-xl drop-shadow-md w-80 m-auto leading-5'>¿Buscas un sitio web que represente a tu negocio?</p>
     <h5 className='w-60 mx-auto mt-12 text-center text-cyan-700 font-montserrat font-extrabold text-6xl tracking-tighter leading-9'>Modo <br/> <span className='text-verde-900 font-semibold pl-10 relative top-3'>digital</span></h5>
     <h1 className='w-96 mt-5 mx-auto text-center text-white opacity-85 font-montserrat font-bold text-5xl tracking-tight leading-9 drop-shadow-md'>Diseño web <br/>profesional</h1>
---
+++ cliente/src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <div class="w-screen absolute py-3 px-3">
+        <img className='w-12' src="./logo.png" alt="Logo ModoDigital" />
+    </div>
+  );
+};
+
+export default NavBar;
