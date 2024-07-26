cb4d05027f64c984c1937c78d9b95aaccc361cfd
Matias <mati.dehoyosmdp@gmail.com>
2024-07-25 21:32:23 -0300

    first

---
+++ cliente/tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+      colors: {
+        'azul': {
+          900: '#0EADB9'
+        },
+        'verde': {
+          900: '#15E097'
+        }
+    },
+  variants: {},
+  plugins: [],
+}
+}
+}
