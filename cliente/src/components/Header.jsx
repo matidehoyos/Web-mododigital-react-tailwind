67805ac7b1498d314542424630bfa4b773f3fcc1
Matias <mati.dehoyosmdp@gmail.com>
2024-07-26 19:37:16 -0300

    header

--- cliente/src/components/Header.jsx
+++ cliente/src/components/Header.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 
 const Header = () => {
   return (
-<div class="bg-[url('/bg.jpeg')] bg-bottom bg-cover min-h-screen w-screen pt-12">
-    <p className='font-semibold tracking-[.03em] text-white opacity-80 text-center text-xl drop-shadow-md w-80 m-auto leading-5'>¿Buscas un sitio web que represente a tu negocio?</p>
-    <h5 className='w-60 mx-auto mt-12 text-center text-cyan-700 font-montserrat font-extrabold text-6xl tracking-tighter leading-9'>Modo <br/> <span className='text-verde-900 font-semibold pl-10 relative top-3'>digital</span></h5>
-    <h1 className='w-96 mt-5 mx-auto text-center text-white opacity-85 font-montserrat font-bold text-5xl tracking-tight leading-9 drop-shadow-md'>Diseño web <br/>profesional</h1>
-    <h1 className='w-80 mt-14 mx-auto text-center text-white opacity-90 font-montserrat text-lg -tracking-tight drop-shadow-md leading-none'>Suma prestigio, jerarquia y posicionamiento para tu negocio con un software hecho a tu medida.</h1>
-    <button className='p-0 w-auto mt-16 mx-36 -tracking-tight rouded-md'><a href='' className='bg-red-500 opacity-80 py-2 px-2 rounded-md shadow-2xl shadow-red-900'>Comencemos</a></button>
-    </div>
+        <div class="bg-[url('/bg1.jpeg')] bg-bottom bg-cover min-h-screen w-screen pt-20">
+                <p className='font-normal tracking-[.03em] text-white opacity-60 text-center text-lg drop-shadow-md w-60 m-auto leading-5'>¿Buscas un sitio web que represente a tu negocio?</p>
+                <img src="./marca.png" alt="Logo Modo Digital." className='w-60 mx-auto mt-16' />
+                <h1 className='w-96 mt-1 mx-auto text-center text-white opacity-85 font-montserrat font-bold text-5xl tracking-tighter leading-10 drop-shadow-md'>Diseño web <br/>profesional</h1>
+                <p className='w-11/12 mt-16 mx-auto text-pretty text-center text-white opacity-80 font-montserrat text-xl drop-shadow-md leading-5'>Suma prestigio, jerarquia y posicionamiento para tu negocio con un software hecho a tu medida.</p>
+                <div class="flex justify-center mt-12">
+                     <a href="#contacto">
+                        <button class="bg-red-800 text-white opacity-70 py-2 px-4 rounded-md shadow-2xl shadow-red-600 leading-0 tracking-tighter">Comencemos</button>
+                    </a>
+                </div>
+        </div>
   );
 };
 
--- cliente/src/components/NavBar.jsx
+++ cliente/src/components/NavBar.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const NavBar = () => {
   return (
-    <div class="w-screen absolute py-2 px-3">
-        <img className='w-8' src="./logo.png" alt="Logo ModoDigital" />
+    <div class="w-screen absolute pt-5 px-3">
+        <img className='w-10' src="./logo.png" alt="Logo ModoDigital" />
     </div>
   );
 };
