f7b9345fd03c11b1563b0ca55d1e8050500e96a6
Matias <mati.dehoyosmdp@gmail.com>
2024-07-27 16:28:40 -0300

    header

--- cliente/src/components/Header.jsx
+++ cliente/src/components/Header.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Header = () => {
   return (
-        <div class="bg-[url('/bg2.jpeg')] bg-bottom bg-cover h-auto w-screen pt-16 pb-28">
+        <div class="bg-[url('/bg2.jpeg')] bg-bottom bg-cover h-auto w-screen pt-16 pb-36">
                 <p className='tracking-[.03em] text-slate-200 text-center text-[16px] md:text- font-normal drop-shadow-sm w-60 m-auto leading-5'>¿Buscas un sitio web de categoria para tu negocio?</p>
                 <img src="./marca.png" alt="Logo Modo Digital." className='w-60 mx-auto mt-14' />
-                <h1 className='w-[70%] mx-auto bg-black bg-opacity-15 text-center text-white opacity-95 font-montserrat text-5xl tracking-tight leading-10 drop-shadow-md'>Diseño web <br/>profesional</h1>
+                <h1 className='w-[70%] mx-auto text-center text-white opacity-95 font-montserrat text-5xl tracking-tight leading-10 drop-shadow-md'>Diseño web <br/>profesional</h1>
                 <p className='w-4/5 mt-14 mx-auto py-4 text-pretty text-center text-azul-900 opacity-70 font-montserrat text-xl font-bold drop-shadow-md leading-6 rounded-lg'>Suma prestigio, jerarquia y posición a tu negocio con un software hecho a su medida.</p>
                 <div class="flex justify-center mt-14">
                      <a href="#contacto">
-                        <button class="bg-gradient-to-r to-blue-500 from-green-500 text-white py-2 px-3 rounded-md shadow-2xl shadow-white leading-0 tracking-tighter">Comencemos</button>
+                        <button class="text-white py-2 px-3 border rounded-md shadow-2xl shadow-white leading-0 tracking-tighter">Comencemos</button>
                     </a>
                 </div>
         </div>
--- cliente/src/components/Tipos.jsx
+++ cliente/src/components/Tipos.jsx
@@ -3,9 +3,8 @@ import React from 'react';
 const Tipos = () => {
   return (
     <div class="w-screen bg-slate-200 pb-20">
-        <div className='w-[90%] m-auto bg-azul-900 py-6 px-2 rounded-lg shadow-lg shadow-slate-500 relative -top-14 bg-opacity-90'>
-            <p className='text-white text-[24px] text-center font-bold leading-7'>Creamos software adaptable, a medida y auto administrable.</p>
-            <p className='text-slate-200 text-[20px] text-center text-pretty font-semibold tracking-wide leading-[22px] mt-4'>Nos adaptamos a la imagen de tu marca, creando un diseño que la impulse, y autoadministrable, para que tengas el total control.</p>
+        <div className='w-[80%] m-auto  py-8 px-6 bg-azul-900 rounded-md shadow-lg shadow-gray-600 relative -top-20'>
+            <p className='text-slate-200 text-[22px] text-center break-all text-pretty font-normal tracking-wide drop-shadow leading-[22.5px]'>Nos adaptamos a la imagen de tu marca, creando un diseño que la impulse, y autoadministrable, para que tengas el total control.</p>
         </div>
         <h6 className='font-bold text-center text-[18px] text-neutral-800'>¿Que tipos de webs creamos?</h6>
     </div>
