03f752b16a59edaec7a7527dfd795244c916c12f
Matias <mati.dehoyosmdp@gmail.com>
2024-07-27 17:27:49 -0300

    cambios

--- cliente/src/components/Header.jsx
+++ cliente/src/components/Header.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Header = () => {
   return (
-        <div class="bg-[url('/bg2.jpeg')] bg-bottom bg-cover h-auto w-screen pt-16 pb-20">
-                <p className='tracking-[.03em] text-slate-200 text-center text-[18px] opacity-80 font-semibold drop-shadow-sm w-60 m-auto leading-5'>¿Buscas un sitio web de categoria para tu negocio?</p>
+        <div class="bg-[url('/bg2.jpeg')] bg-bottom bg-cover h-auto w-screen pt-24 pb-20">
+                <p className='w-80 m-auto tracking-[0.4px] text-slate-400 text-center text-[22px] font-semibold drop-shadow-sm leading-6'>¿Buscas un sitio web de categoria para tu negocio?</p>
                 <img src="./marca1.png" alt="Logo Modo Digital." className='w-60 mx-auto mt-14' />
-                <h1 className='w-[90%] mx-auto text-center text-white opacity-95 font-montserrat font-semibold text-5xl tracking-tight leading-10 drop-shadow-md'>Diseño web <br/>profesional</h1>
-                <p className='w-[84%] mt-8 mx-auto py-4 text-pretty text-center text-slate-200 opacity-90 drop-shadow-sm font-montserrat text-xl font-bold leading-6 rounded-lg'>Suma prestigio, jerarquia y posición a tu negocio con un software hecho <span className='text-azul-900'>a su medida.</span></p>
-                <div class="flex justify-center mt-10">
+                <h1 className='mx-auto text-center text-slate-200 font-montserrat font-semibold text-5xl tracking-[-1.1px] leading-10 drop-shadow-md'>Diseño web<br/>profesional</h1>
+                <p className='w-[84%] mt-20 mx-auto py-4 text-pretty text-center text-slate-200 opacity-90 drop-shadow-sm font-montserrat text-xl font-bold leading-6 rounded-lg'>Suma prestigio, jerarquia y posición a tu negocio con un software hecho <span className='text-azul-900'>a su medida.</span></p>
+                <div class="flex justify-center mt-16">
                      <a href="#contacto" className='bg-transparent'> 
-                        <button class="text-verde-900 py-2 px-3 bg-transparent border border-verde-900 rounded-md shadow-lg shadow-slate-700 leading-0 tracking-tighter">Comencemos</button>
+                        <button class="bg-verde-900 bg-opacity-60 text-white py-2 px-3 font-semibold rounded-md shadow-lg shadow-slate-600 leading-0 tracking-[0.6px]">Comencemos</button>
                     </a>
                 </div>
         </div>
