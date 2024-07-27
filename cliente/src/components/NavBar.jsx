dccd12ed57efacf0cfa4d19f94edd1d7edfba773
Matias <mati.dehoyosmdp@gmail.com>
2024-07-26 22:24:13 -0300

    tipos

--- cliente/src/components/NavBar.jsx
+++ cliente/src/components/NavBar.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const NavBar = () => {
   return (
-    <div class="w-screen absolute pt-5 px-3">
-        <img className='w-10' src="./logo.png" alt="Logo ModoDigital" />
+    <div class="w-auto absolute pt-5">
+        <img className='w-8 ml-3' src="./logo.png" alt="Logo ModoDigital" />
     </div>
   );
 };
--- cliente/src/components/Tipos.jsx
+++ cliente/src/components/Tipos.jsx
@@ -3,10 +3,11 @@ import React from 'react';
 const Tipos = () => {
   return (
     <div class="w-screen bg-slate-200 pb-20">
-        <div className='w-[86%] m-auto bg-cyan-700 py-10 px-4 rounded-lg shadow-xl shadow-black relative -top-14 bg-opacity-90'>
-            <p className='text-white text-[21px] text-center font-bold leading-6'>Creamos software personalizado y autoadministrable.</p>
-            <p className='text-white text-[20px] text-center font-normal leading-[22px] mt-4'>Nos adaptamos a la imagen de tu marca, creando un diseño que la impulse, y autoadministrable, para que tengas el total control.</p>
+        <div className='w-[90%] m-auto bg-azul-900 py-6 px-2 rounded-lg shadow-lg shadow-slate-500 relative -top-14 bg-opacity-90'>
+            <p className='text-white text-[24px] text-center font-bold leading-7'>Creamos software adaptable, a medida y auto administrable.</p>
+            <p className='text-slate-200 text-[20px] text-center text-pretty font-semibold tracking-wide leading-[22px] mt-4'>Nos adaptamos a la imagen de tu marca, creando un diseño que la impulse, y autoadministrable, para que tengas el total control.</p>
         </div>
+        <h6 className='font-bold text-center text-[18px] text-neutral-800'>¿Que tipos de webs creamos?</h6>
     </div>
   );
 };
