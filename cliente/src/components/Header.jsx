72e92145c602d8fa6d513cbd2607abb77fe546fb
Matias <mati.dehoyosmdp@gmail.com>
2024-07-27 17:06:18 -0300

    header

--- cliente/src/components/Header.jsx
+++ cliente/src/components/Header.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Header = () => {
   return (
-        <div class="bg-[url('/bg2.jpeg')] bg-bottom bg-cover h-auto w-screen pt-16 pb-36">
+        <div class="bg-[url('/bg2.jpeg')] bg-bottom bg-cover h-auto w-screen pt-16 pb-20">
                 <p className='tracking-[.03em] text-slate-200 text-center text-[18px] opacity-80 font-semibold drop-shadow-sm w-60 m-auto leading-5'>¿Buscas un sitio web de categoria para tu negocio?</p>
                 <img src="./marca1.png" alt="Logo Modo Digital." className='w-60 mx-auto mt-14' />
                 <h1 className='w-[90%] mx-auto text-center text-white opacity-95 font-montserrat font-semibold text-5xl tracking-tight leading-10 drop-shadow-md'>Diseño web <br/>profesional</h1>
-                <p className='w-[84%] mt-14 mx-auto py-4 text-pretty text-center text-slate-300 opacity-90 font-montserrat text-xl font-bold drop-shadow-md leading-6 rounded-lg'>Suma prestigio, jerarquia y posición a tu negocio con un software hecho <span className='text-verde-900'>a su medida.</span></p>
-                <div class="flex justify-center mt-14">
-                     <a href="#contacto">
-                        <button class="text-white py-2 px-3 border rounded-md shadow-2xl shadow-white leading-0 tracking-tighter">Comencemos</button>
+                <p className='w-[84%] mt-8 mx-auto py-4 text-pretty text-center text-slate-200 opacity-90 drop-shadow-sm font-montserrat text-xl font-bold leading-6 rounded-lg'>Suma prestigio, jerarquia y posición a tu negocio con un software hecho <span className='text-azul-900'>a su medida.</span></p>
+                <div class="flex justify-center mt-10">
+                     <a href="#contacto" className='bg-transparent'> 
+                        <button class="text-verde-900 py-2 px-3 bg-transparent border border-verde-900 rounded-md shadow-lg shadow-slate-700 leading-0 tracking-tighter">Comencemos</button>
                     </a>
                 </div>
         </div>
--- cliente/src/components/Tipos.jsx
+++ cliente/src/components/Tipos.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Tipos = () => {
   return (
     <div class="w-screen bg-slate-200 pb-20">
-        <div className='w-[80%] m-auto  py-8 px-6 bg-azul-900 rounded-md shadow-lg shadow-gray-600 relative -top-20'>
-            <p className='text-slate-200 text-[22px] text-center break-all text-pretty font-normal tracking-wide drop-shadow leading-[22.5px]'>Nos adaptamos a la imagen de tu marca, creando un diseño que la impulse, y autoadministrable, para que tengas el total control.</p>
+        <div className='w-[92%] m-auto  py-6 px-4 bg-azul-900 rounded-md shadow-lg shadow-gray-600 relative -top-8'>
+            <p className='text-slate-200 text-[20px] text-center break-all text-pretty font-semibold tracking-wide drop-shadow leading-[22.5px]'>Nos adaptamos a la imagen de tu marca, creando un diseño que la impulse, y autoadministrable, para que tengas el total control.</p>
         </div>
         <h6 className='font-bold text-center text-[18px] text-neutral-800'>¿Que tipos de webs creamos?</h6>
     </div>
