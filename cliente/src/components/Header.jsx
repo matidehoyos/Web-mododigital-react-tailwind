6cacfad23d25049931fc5806c85e28547b51547a
Matias <mati.dehoyosmdp@gmail.com>
2024-07-28 20:01:21 -0300

    herder

--- cliente/src/components/Header.jsx
+++ cliente/src/components/Header.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Header = () => {
   return (
-        <div class="bg-[url('/bg3.jpeg')] bg-bottom bg-cover h-auto w-screen pt-24 pb-20">
-                <p className='w-[80%] m-auto tracking-[0.4px] text-slate-300 text-center text-[20px] font-semibold drop-shadow-sm leading-6'>¿Buscas un sitio web de categoria para tu negocio?</p>
+        <div class="bg-[url('/bg3.jpeg')] bg-cover h-auto w-screen pt-24 pb-20">
+                <p className='w-[80%] m-auto tracking-[0.4px] text-slate-300 text-center text-[20px] font-semibold drop-shadow-sm leading-6 break-none'>¿Buscas un sitio web de categoria para tu negocio?</p>
                 <img src="./marca1.png" alt="Logo Modo Digital." className='w-60 mx-auto mt-14' />
                 <h1 className='mx-auto text-center text-slate-200 font-montserrat font-semibold text-5xl tracking-[-1.1px] leading-10 drop-shadow-md'>Diseño web<br/>profesional</h1>
                 <p className='w-[84%] mt-20 mx-auto py-4 text-pretty text-center text-slate-200 opacity-90 drop-shadow-sm font-montserrat text-xl font-bold leading-6 rounded-lg'>Suma prestigio, jerarquia y posición a tu negocio con un software hecho <span className='text-azul-900'>a su medida.</span></p>
