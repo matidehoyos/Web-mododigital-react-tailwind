db2ee3ec79865e2cb8e7c45d51720146a95a76d6
Matias <mati.dehoyosmdp@gmail.com>
2024-07-29 21:35:24 -0300

    prueba

--- cliente/src/components/Header.jsx
+++ cliente/src/components/Header.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Header = () => {
   return (
-        <div class="bg-[url('/bg4.jpeg')] bg-cover h-auto w-screen pt-24 pb-20">
-                <p className='w-[70%] m-auto tracking-[0.4px] text-slate-300 text-center text-[20px] font-semibold drop-shadow-sm leading-6 break-keep'>¿Buscas un sitio web de categoria para tu negocio?</p>
-                <img src="./marca1.png" alt="Logo Modo Digital." className='w-60 mx-auto mt-14' />
-                <h1 className='mx-auto text-center text-slate-200 font-montserrat font-semibold text-5xl tracking-[-1.1px] leading-10 drop-shadow-md'>Diseño web<br/>profesional</h1>
+        <div className="h-auto w-screen pt-24 pb-20 bg-gradient-to-r from-azul-900 to-verde-900">
+                <p className='w-auto m-auto tracking-[0.4px] text-slate-300 text-center text-[22px] font-bold drop-shadow-sm leading-6 break-keep'>¿Buscas un sitio web de categoria?</p>
+                <img src="./marca1.png" alt="Logo Modo Digital." className='w-[70%] mx-auto mt-14 px-4 bg-white bg-opacity-40 rounded-md' />
+                <h1 className='mx-auto mt-2 text-center text-slate-100 font-montserrat font-[600] text-[46px] tracking-[-1.1px] leading-10 drop-shadow-md'>Diseño web<br/>profesional</h1>
                 <p className='w-[84%] mt-20 mx-auto py-4 text-pretty text-center text-slate-200 opacity-90 drop-shadow-sm font-montserrat text-xl font-bold leading-6 rounded-lg'>Suma prestigio, jerarquia y posición a tu negocio con un software hecho <span className='text-azul-900'>a su medida.</span></p>
                 <div class="flex justify-center mt-16">
                      <a href="#contacto" className='bg-transparent'> 
--- cliente/src/components/NavBar.jsx
+++ cliente/src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NavBar = () => {
   return (
-    <div class="w-auto absolute pt-5">
+    <div class="w-screen absolute py-5 bg-slate-100">
         <img className='w-8 ml-3' src="./logo.png" alt="Logo ModoDigital" />
     </div>
   );
