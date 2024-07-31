722d53d43e27f42cc41dafd4cc8527d93e40a11f
Matias <mati.dehoyosmdp@gmail.com>
2024-07-31 10:39:18 -0300

    herder

--- cliente/src/components/NavBar.jsx
+++ cliente/src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NavBar = () => {
   return (
-    <div class="w-screen absolute py-3">
+    <div class="w-screen absolute py-3 z-40">
         <img className='w-8 ml-3' src="./log.png" alt="Logo ModoDigital" />
     </div>
   );
