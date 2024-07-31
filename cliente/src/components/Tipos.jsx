3f72dad7f6e542086544c6ab9e6c94238dee1168
Matias <mati.dehoyosmdp@gmail.com>
2024-07-31 12:25:10 -0300

    ok

--- cliente/src/components/Tipos.jsx
+++ cliente/src/components/Tipos.jsx
@@ -4,7 +4,7 @@ import { FaCheck } from 'react-icons/fa'
 const Tipos = () => {
   return (
     <div class="w-screen pt-[80px] bg-white">
-        <h2 className='font-extrabold text-center text-[22px] text-verde-900 drop-shadow'>¿Que tipos de webs creamos?</h2>
+        <h2 className='font-extrabold text-center text-[20px] text-black opacity-85 drop-shadow'>¿Que tipos de webs creamos?</h2>
         <div className='w-screen flex-col pt-[60px]'>
             <div className='mb-12 pl-[20px] flex-col justify-start items-start'>
                 <img src="./ecommerce.png" className='w-[26%]' alt="Ilustracion de comercio electronico." />
@@ -23,14 +23,14 @@ const Tipos = () => {
             </div>
         </div>
         <div className='w-screen h-auto mt-20'>
-            <p className='w-screen mx-auto py-28 px-2 text-white font-bold text-center text-[22px] bg-[url(/diseno.jpeg)] bg-cover bg-center'>
+            <p className='w-screen mx-auto py-28 px-2 text-white font-bold text-center text-[22px] drop-shadow bg-[url(/diseno.jpeg)] bg-cover bg-center'>
               Diseños interactivos, innovadores y elegantes.
             </p>
         </div>
         <div className='w-screen py-16 px-[15px]'>
               <h4 className='text-[24px] text-black opacity-90 font-bold'>Ecommerce</h4>
-              <p className='text-[18px] text-black opacity-80 font-bold'>Que incluye?</p>
-              <ul class="mt-1 pt-4 list-none text-[18px] font-semibold drop-shadow text-black opacity-90 tracking-tight border-t border-black border-opacity-60">
+              <p className='text-[18px] text-black opacity-70 font-bold'>Que incluye?</p>
+              <ul class="mt-1 pt-4 list-none text-[18px] font-semibold drop-shadow text-black opacity-80 tracking-tight border-t border-black border-opacity-60">
                 <li class="flex items-baseline mb-[2px]"><FaCheck class="mr-1 text-verde-900 text-[10px]" />Panel de control autoadministrable.</li>
                 <li class="flex items-baseline mb-[2px]"><FaCheck class="mr-1 text-verde-900 text-[10px]"/>Productos y categorias ilimitados.</li>
                 <li class="flex items-baseline mb-[2px]"><FaCheck class="mr-1 text-verde-900 text-[10px]" />Metodos de pago en linea.</li>
