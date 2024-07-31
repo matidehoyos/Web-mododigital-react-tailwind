eaa31c569ce794eeb834df4d4d8aee841f7e48d5
Matias <mati.dehoyosmdp@gmail.com>
2024-07-31 20:54:32 -0300

    bg

--- cliente/src/components/Emprend.jsx
+++ cliente/src/components/Emprend.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Emprend = () => {
   return (
-    <div class="w-screen py-16 px-[15px] bg-white bg-opacity-30">
+    <div class="w-screen py-16 px-[15px] bg-[rgb(244,244,244)]">
           <h4 className='text-[22px] text-azul-900 tracking-wide leading-[28px] font-bold'>SITIOS PARA <br/>EMPRENDIMIENTOS</h4>
           <p className='w-[98%] mt-3 text-gray-900 text-[17px] leading-[22px] tracking-wide font-medium drop-shadow'>Date a conocer frente a tus potenciales clientes, generando confianza y ganando prestigio, con un sitio web que grafique tu actividad, muestre tu producto, revele la  informacion importante de tu emprendimiento, reuna las redes sociales y canales de difusion. Mantenete siempre actualizado y en comunicacion directa con tu publico. <br/> </p>
     </div>
--- cliente/src/components/Header.jsx
+++ cliente/src/components/Header.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const Header = () => {
   return (
         <div className="bg-[url('/bg5.jpeg')] bg-cover bg-top h-auto w-screen pt-[75px] pb-[90px]">
-                <p className='m-auto tracking-[0.6px] text-white opacity-95 text-center text-[20px] font-bold drop-shadow'>¿Necesitas un sitio web?</p>
-                <img src="./marca1.png" alt="Logo Modo Digital." className='w-[64%] mx-auto mt-6 bg-black bg-opacity-20' />
-                <h1 className='mx-auto text-center text-white font-[800] text-[54px] tracking-[-.8px] leading-[40px] drop-shadow'>Diseño web<br/><span className='font-[300] text-[36px] relative -top-[8px] left-[66px]'>profesional</span></h1>
-                <p className='w-[90%] mt-32 mx-auto text-pretty text-center text-white opacity-90 drop-shadow text-[21px] font-semibold leading-[26px] bg-black bg-opacity-20'>Prestigio, jerarquia y posición con un software hecho <span className='text-verde-900'>a su medida.</span></p>
+                <p className='m-auto tracking-[0.6px] text-white opacity-95 text-center text-[18px] font-medium drop-shadow'>¿Necesitas un sitio web?</p>
+                <img src="./marca1.png" alt="Logo Modo Digital." className='w-[60%] mx-auto mt-6 bg-black bg-opacity-20 relative -left-[20px]' />
+                <h1 className='mx-auto text-center text-white font-[800] text-[54px] tracking-[-.8px] leading-[40px] drop-shadow'>Diseño web<br/><span className='font-[300] text-[32px] relative -top-[8px] left-[50px]'>profesional</span></h1>
+                <p className='w-[90%] mt-32 mx-auto text-pretty text-center text-white opacity-90 drop-shadow text-[21px] font-semibold leading-[26px] bg-black bg-opacity-30'>Prestigio, jerarquia y posición con un software hecho <span className='text-verde-900'>a su medida.</span></p>
         </div>
   );
 };
