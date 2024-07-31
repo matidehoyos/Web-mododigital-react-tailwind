9271e550a9e6d3917955f54e4f2763ba6191103f
Matias <mati.dehoyosmdp@gmail.com>
2024-07-31 19:32:01 -0300

    correccion

--- cliente/src/components/NavBar.jsx
+++ cliente/src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NavBar = () => {
   return (
-    <div class="w-screen absolute py-3 z-40">
+    <div class="w-screen absolute pt-5 z-40">
         <img className='w-8 ml-3' src="./logo.png" alt="Logo ModoDigital" />
     </div>
   );
