4e2b2c2f6db8aa1a7339aa400de7668156443a84
Matias <mati.dehoyosmdp@gmail.com>
2024-08-01 12:11:15 -0300

    tienda

--- cliente/src/components/Header.jsx
+++ cliente/src/components/Header.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 const Header = () => {
   return (
         <div className="bg-[url('/bg.jpeg')] bg-cover bg-top h-[100vh] w-screen">
-                <div className='w-screen h-[100vh] bg-black opacity-45 absolute'></div>
-                <p className='m-auto pt-[90px] tracking-[0.6px] text-white opacity-80 text-center text-[18px] font-montserrat font-bold italic drop-shadow-sm'>¿Buscas una web a tu medida?</p>
+                <div className='w-screen h-[100vh] bg-black opacity-35 absolute'></div>
+                <p className='m-auto pt-[90px] tracking-[0.3px] text-white opacity-80 text-center text-[18px] font-montserrat font-extrabold italic drop-shadow-sm'>¿Buscas una web a tu medida?</p>
                 <img src="./marca1.png" alt="Logo Modo Digital." className='w-[72%] mx-auto mt-24 opacity-75' />
                 <h1 className='mx-auto text-center text-white font-[600] text-[52px] font-montserrat tracking-[-.8px] leading-[40px] drop-shadow opacity-90'>Diseño web<br/><span className='font-[300] text-[32px] relative -top-[8px] left-[56px] opacity-50'>profesional</span></h1>
-                <p className='w-[82%] mt-32 mx-auto text-pretty text-center text-white opacity-80 drop-shadow text-[20px] font-semibold leading-[24px]'>Dale prestigio y credibilidad a tu negocio con un sitio a su medida.</p>
+                <p className='w-[82%] mt-32 mx-auto text-pretty text-center text-white opacity-80 drop-shadow text-[24px] font-semibold leading-[24px]'>Dale prestigio y credibilidad a tu negocio con un sitio a su medida.</p>
         </div>
   );
 };
--- cliente/src/components/Tipos.jsx
+++ cliente/src/components/Tipos.jsx
@@ -3,28 +3,28 @@ import { FaCheck } from 'react-icons/fa'
 
 const Tipos = () => {
   return (
-    <div class="w-screen py-[50px] bg-[rgb(244,244,244)]">
-        <h2 className='font-bold text-center text-[20px] tracking-wide text-gray-800'>¿Que tipos de webs creamos?</h2>
+    <div class="w-screen py-[50px] bg-gradient-to-tl from-azul-900 to-gray-500">
+        <h2 className='font-bold text-center text-[20px] tracking-wide text-white drop-shadow'>¿Que tipos de webs creamos?</h2>
         <div className='w-screen flex-col pt-[40px]'>
-            <div className='w-[84%] mx-auto mb-6 py-[4px] pl-[12px] pb-4 flex-col justify-start items-start bg-gray-200 rounded-sm'>
+            <div className='w-[84%] mx-auto mb-6 py-[4px] pl-[12px] pb-4 flex-col justify-start items-start bg-gray-100 bg-opacity-20 rounded-sm border border-verde-900 border-opacity-25'>
                 <img src="./ecommerce.png" className='w-[22%]' alt="Ilustracion de comercio electronico." />
                 <h3 className='pt-2 text-[20px] text-gray-900 drop-shadow font-bold'>E-commerce</h3>
                 <p className='w-[90%] pt-1 text-[18px] text-gray-800 font-bold tracking-tight leading-[20px] opacity-95'>Vende por vos las 24hs. Carrito de compras y metodos de pago.</p>
             </div>
-            <div className='w-[84%] mx-auto mb-6 py-[4px] pl-[12px] pb-4 flex-col justify-start items-start bg-gray-200 rounded-sm'>
+            <div className='w-[84%] mx-auto mb-6 py-[4px] pl-[12px] pb-4 flex-col justify-start items-start bg-gray-100 bg-opacity-20 rounded-sm border border-verde-900 border-opacity-25'>
                 <img src="./institucional.png" className='w-[23%]' alt="Ilustracion de web institucional." />
                 <h3 className='pt-2 text-[20px] text-gray-900 drop-shadow  font-bold'>Institucional</h3>
                 <p className='w-[90%] pt-1 text-[18px] text-gray-800 font-bold tracking-tight leading-[20px] opacity-95'>Reune la informacion relevante del negocio, club, etc. Redes sociales, medios de contacto, blog, canal de difusion.</p>
             </div>
-            <div className='w-[84%] mx-auto mb-10 py-[4px] pl-[20px] pb-4 flex-col justify-start items-start bg-gray-200 rounded-sm'>
+            <div className='w-[84%] mx-auto mb-6 py-[4px] pl-[12px] pb-4 flex-col justify-start items-start bg-gray-100 bg-opacity-20 rounded-sm border border-verde-900 border-opacity-25'>
                 <img src="./landing.png" className='w-[23%]' alt="Ilustracion de  landing page." />
                 <h3 className='pt-2 text-[20px] text-gray-900 drop-shadow font-bold'>Landing page</h3>
                 <p className='w-[90%] pt-1 text-[18px] text-gray-800 font-bold tracking-tight leading-[20px] opacity-95'>Ideales para campañas publicitarias. Destinadas a un público y objetivo especifico. Convierte visitantes en clientes.</p>
             </div>
-            <div className='w-[84%] mx-auto py-[40px] rounded-sm border border-azul-900'>
-                <h3 className='text-[26px] tracking-wide leading-[42px] font-[800] text-center text-gray-700 drop-shadow border border-azul-900'>BLOGS</h3>
-                <h3 className='text-[26px] tracking-wide leading-[42px] font-[800] text-center  text-gray-700 drop-shadow border border-azul-900'>PORTFOLIOS</h3>
-                <h3 className='text-[26px] tracking-wide leading-[42px] font-[800] text-center text-gray-700 drop-shadow border border-azul-900'>SOCIAL WEBS</h3>
+            <div className='w-[84%] mx-auto py-[40px] rounded-sm border border-verde-900'>
+                <h3 className='text-[24px] tracking-wide leading-[40px] font-[700] text-center text-white opacity-75 drop-shadow border border-verde-900'>BLOGS</h3>
+                <h3 className='text-[24px] tracking-wide leading-[40px] font-[700] text-center  text-white opacity-75 drop-shadow border border-verde-900'>PORTFOLIOS</h3>
+                <h3 className='text-[24px] tracking-wide leading-[40px] font-[700] text-center text-white opacity-75 drop-shadow border border-verde-900'>SOCIAL WEBS</h3>
             </div>
         </div>
     </div>
