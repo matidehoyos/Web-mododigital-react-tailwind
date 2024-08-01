1740e78be034e55b1d0953b963b34dbc52436d54
Matias <mati.dehoyosmdp@gmail.com>
2024-08-01 13:35:39 -0300

    b

--- cliente/src/components/Ecommerce.jsx
+++ cliente/src/components/Ecommerce.jsx
@@ -3,10 +3,10 @@ import { FaCheck } from 'react-icons/fa';
 
 const Ecommerce = () => {
   return (
-    <div className='w-screen py-16 px-[15px] bg-gray-300'>
-    <img src="./eco.png" alt="imagen pagina web" className='w-[26%] absolute right-10' />    
+    <div className='w-screen py-16 px-[15px] bg-gray-200'>
+    <img src="./eco.png" alt="imagen pagina web" className='w-[40%] mb-4' />    
     <h4 className='text-[24px] text-gray-800 opacity-90 font-bold'>Ecommerce</h4>
-    <p className='text-[18px] text-gray-700 opacity-70 font-bold'>Que incluye?</p>
+    <p className='text-[18px text-gray-700 opacity-70 font-bold'>Que incluye?</p>
     <ul class="mt-1 pt-4 list-none text-[16px] font-semibold drop-shadow text-gray-700 tracking-tight border-t border-white border-opacity-60">
       <li class="flex items-baseline mb-[2px]"><FaCheck class="mr-1 text-verde-900 text-[10px]" />Panel de control autoadministrable.</li>
       <li class="flex items-baseline mb-[2px]"><FaCheck class="mr-1 text-verde-900 text-[10px]"/>Productos y categorias ilimitados.</li>
--- cliente/src/components/Tipos.jsx
+++ cliente/src/components/Tipos.jsx
@@ -6,33 +6,34 @@ const Tipos = () => {
     <div class="w-screen py-[50px] bg-gradient-to-tl from-white to-gray-200">
         <h2 className='font-bold text-center text-[20px] tracking-wide text-gray-800 drop-shadow'>¿Que tipos de webs creamos?</h2>
         <div className='w-screen flex-col pt-[40px]'>
-            <div className='w-[84%] mx-auto mb-6 flex justify-start items-center rounded-sm bg-gray-300'>
-                <img src="./ecommerce.jpeg" className='w-[26%]' alt="Ilustracion de comercio electronico." />
+            <div className='w-[84%] mx-auto mb-6 flex justify-start items-center rounded-sm bg-gray-200'>
+                <img src="./ecommerce.jpeg" className='w-[42%] grayscale-[40%]' alt="Ilustracion de comercio electronico." />
                 <div className='pl-3'>
-                  <h3 className='text-[20px] text-gray-900 drop-shadow font-bold'>E-commerce</h3>
-                  <p className='text-[16px] text-gray-900 font-bold tracking-tight leading-[20px] opacity-80'>Vende por vos las 24hs.</p>
+                  <h3 className='text-[20px] text-gray-800 drop-shadow font-bold'>E-commerce</h3>
+                  <p className='text-[16px] text-gray-800 font-bold tracking-tight leading-[20px] opacity-80'>Vende por vos las 24hs.</p>
                 </div>
             </div>
-            <div className='w-[84%] mx-auto mb-6 flex justify-start items-center rounded-sm bg-gray-300'>
-                <img src="./insti.jpeg" className='w-[26%]' alt="Ilustracion de comercio electronico." />
+            <div className='w-[84%] mx-auto mb-6 flex justify-start items-center rounded-sm bg-gray-200'>
+                <img src="./insti.jpeg" className='w-[42%] grayscale-[40%]' alt="Ilustracion de comercio electronico." />
                 <div className='pl-3'>
-                  <h3 className='text-[20px] text-gray-900 drop-shadow font-bold'>Institucional</h3>
-                  <p className='text-[16px] text-gray-900 font-bold tracking-tight leading-[20px] opacity-80'>Reune la info de tu negocio.</p>
+                  <h3 className='text-[20px] text-gray-800 drop-shadow font-bold'>Institucional</h3>
+                  <p className='text-[16px] text-gray-800 font-bold tracking-tight leading-[20px] opacity-80'>Reune la informacion del negocio.</p>
                 </div>
             </div>
-            <div className='w-[84%] mx-auto mb-6 flex justify-start items-center rounded-sm bg-gray-300'>
-                <img src="./land.jpeg" className='w-[26%]' alt="Ilustracion de comercio electronico." />
+            <div className='w-[84%] mx-auto mb-6 flex justify-start items-center rounded-sm bg-gray-200'>
+                <img src="./land.jpeg" className='w-[42%] grayscale-[40%]' alt="Ilustracion de comercio electronico." />
                 <div className='pl-3'>
-                  <h3 className='text-[20px] text-gray-900 drop-shadow font-bold'>Landing page</h3>
-                  <p className='text-[16px] text-gray-900 font-bold tracking-tight leading-[20px] opacity-80'>Ideal para campaña publicitaria.</p>
+                  <h3 className='text-[20px] text-gray-800 drop-shadow font-bold'>Landing page</h3>
+                  <p className='text-[16px] text-gray-800 font-bold tracking-tight leading-[20px] opacity-80'>Ideales para campanas publicitarias.</p>
                 </div>
             </div>
-            <div className='w-[84%] mx-auto py-[40px] rounded-sm bg-gray-300'>
+           
+         {/*}   <div className='w-[84%] mx-auto py-[40px] rounded-sm bg-gray-300'>
                 <h3 className='text-[24px] tracking-wide leading-[40px] font-[700] text-center text-gray-800 opacity-75 drop-shadow border border-gray-900'>BLOGS</h3>
                 <h3 className='text-[24px] tracking-wide leading-[40px] font-[700] text-center  text-gray-800 opacity-75 drop-shadow border border-gray-900'>PORTFOLIOS</h3>
                 <h3 className='text-[24px] tracking-wide leading-[40px] font-[700] text-center text-gray-800 opacity-75 drop-shadow border border-gray-900'>SOCIAL WEBS</h3>
-            </div>
-        </div>
+            </div>*/ }
+        </div> 
     </div>
   );
 };
