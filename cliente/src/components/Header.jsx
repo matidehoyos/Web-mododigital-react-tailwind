7ae30e5cb467ca46d53163d53d9130403b564d3a
Matias <mati.dehoyosmdp@gmail.com>
2024-08-02 11:40:29 -0300

    bg

--- cliente/src/components/Header.jsx
+++ cliente/src/components/Header.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 const Header = () => {
   return (
-        <div className="bg-[url('/bg6.jpeg')] bg-cover h-[100vh] w-screen">
-                <p className='m-auto pt-[100px] tracking-[0.3px] text-white opacity-80 text-center text-[18px] font-montserrat font-[600] italic'>¿Buscas un sitio a tu medida?</p>
-                <img src="./marca1.png" alt="Logo Modo Digital." className='w-[64%] mx-auto mt-12 bg-black bg-opacity-10' />
-                <h1 className='mx-auto mt-3 text-center text-white font-[550] text-[46px] font-montserrat tracking-[-.8px] leading-[30px] drop-shadow'>Desarrollo web<br/><span className='opacity-50 text-[32px] font-[600]'>profesional</span></h1>
+        <div className="bg-[url('/bg7.jpg')] bg-contain bg-no-repeat h-[100vh] w-screen">
+                <div className='w-screen h-[100vh] bg-black bg-opacity-65 blur-lg absolute'></div>
+                <p className='m-auto pt-[80px] tracking-[0.3px] text-white opacity-80 text-center text-[18px] font-montserrat font-[600] italic'>¿Buscas un sitio a tu medida?</p>
+                <img src="./marca1.png" alt="Logo Modo Digital." className='w-[64%] mx-auto mt-12 opacity-95 bg-black bg-opacity-10' />
+                <h1 className='mx-auto mt-3 text-center text-white opacity-85 font-[550] text-[46px] font-montserrat tracking-[-.8px] leading-[30px] drop-shadow'>Desarrollo web<br/><span className='opacity-70 text-[32px] font-[600]'>profesional</span></h1>
                 <p className='w-[92%] mt-20 mx-auto p-2 text-pretty text-center text-white opacity-70 drop-shadow text-[22px] font-[600] tracking-[.3px] leading-[25px] font-montserrat italic'>Prestigio, jerarquia y credibilidad para tu negocio.</p>
                 <div className="flex justify-center">
                   <a href="#" className="mt-14 py-[6px] px-[14px] text-white text-[16px] font-[600] tracking-[.3px] drop-shadow border rounded-sm">
--- cliente/src/components/NavBar.jsx
+++ cliente/src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NavBar = () => {
   return (
-    <div class="w-screen absolute py-4 z-40 bg-black bg-opacity-25">
+    <div class="w-screen absolute pt-4 z-10">
         <img className='w-9 ml-[20px] dropshadow' src="./logo.png" alt="Logo ModoDigital"  />
     </div>
   );
