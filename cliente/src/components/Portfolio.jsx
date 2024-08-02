13bdb0d32504404d2f791dfb9fb2d3e7438735a5
Matias <mati.dehoyosmdp@gmail.com>
2024-08-02 17:01:27 -0300

    dfg

--- cliente/src/components/Portfolio.jsx
+++ cliente/src/components/Portfolio.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Portfolio = () => {
   return (
-    <div class="w-screen py-16 px-[15px] pb-[100px] overflow-x-hidden bg-[#092527]">
-          <img src="portfolio.jpeg" alt="imagen website" className='w-[100%] opacity-70 rounded-md' />
-          <h4 className=' mt-[15px] text-[24px] text-[#50c6cc] tracking-wide leading-[28px] font-[600]'>PORTFOLIOS<br/><span className='text-gray-300 text-[20px] leading-[20px]'>PARA PROFESIONALES</span></h4>
-          <p className='w-[98%] mt-[12px] text-white text-[20px] leading-tight tracking-[.8px] font-[300]'>Da a conocer tu negocio frente a tus potenciales clientes, generando confianza y ganando prestigio, con un sitio web que grafique tu actividad, muestre tu producto, revele la  informacion importante de tu emprendimiento, reuna las redes sociales y canales de difusion. Mantenete siempre actualizado y en comunicacion directa con tu publico. <br/> </p>
+    <div class="w-screen py-16 px-[15px] pb-[100px] overflow-x-hidden bg-[#1c3133]">
+          <img src="porfolio1.jpeg" alt="imagen website" className='w-[75%] opacity-80 rounded-md' />
+          <h4 className=' mt-[12px] text-[22px] text-[#b9b9b9] tracking-wide leading-none font-[600]'>PORTFOLIOS<br/><span className='text-[#548589] text-[18px] leading-none'>para profesionales</span></h4>
+          <p className='w-[98%] mt-[12px] text-white text-[20px] leading-tight tracking-[.8px] font-[300]'>Una web que muestre a la perfeccion tus proyectos, tu carrera profesional, tu preparacion, y todo lo que se rediera a lo profesional. <br/> </p>
     </div>
   );
 };
