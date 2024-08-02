324876aeb1039a501e889ac94606f0891aaa775f
Matias <mati.dehoyosmdp@gmail.com>
2024-08-02 17:55:27 -0300

    sd

--- cliente/src/components/Interact.jsx
+++ cliente/src/components/Interact.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const Interact = () => {
   return (
 
-        <div className="w-screen py-[70px] bg-[#e3e3e3]">
-            <div className="w-[80%] mx-auto bg-[url('/diseno.jpeg')] bg-cover bg-center rounded-md">
-                  <h3 className='py-[70px] bg-[#111] bg-cover bg-opacity-20 text-[#ffffffb6] text-[20px] font-[600] text-center leading-[28px] tracking-[.6px] drop-shadow'>Diseños interactivos,<br/>atractivos y elegantes</h3>
+        <div className="w-screen p-[70px] bg-[#e3e3e3]">
+            <div className="w-[100%] bg-[url('/diseno.jpeg')] bg-cover bg-center rounded-sm">
+                  <h3 className='py-[70px] bg-[#111] bg-cover bg-opacity-20 text-[#19deec] text-[20px] font-[700] text-center leading-[28px] tracking-[.8px] drop-shadow'>Diseños interactivos,<br/>atractivos y elegantes</h3>
             </div>  
         </div>
   );
