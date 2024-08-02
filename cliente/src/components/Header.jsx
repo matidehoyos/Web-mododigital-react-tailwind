5bd57a0d04f0ff9c421b34f0c7047aa63f428e91
Matias <mati.dehoyosmdp@gmail.com>
2024-08-02 11:48:01 -0300

    }

--- cliente/src/components/Header.jsx
+++ cliente/src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => {
   return (
         <div className="bg-[url('/bg7.jpg')] bg-cover bg-center bg-no-repeat h-[100vh] w-screen">
-                <div className='w-screen h-[100vh] bg-black bg-opacity-80 blur-md absolute'></div>
+                <div className='w-screen h-[100vh] bg-black bg-opacity-70 blur-sm absolute overflow-hidden'></div>
                 <p className='m-auto pt-[80px] tracking-[0.3px] text-white opacity-80 text-center text-[18px] font-montserrat font-[600] italic'>¿Buscas un sitio a tu medida?</p>
                 <img src="./marca1.png" alt="Logo Modo Digital." className='w-[64%] mx-auto mt-12 opacity-95 bg-black bg-opacity-10' />
                 <h1 className='mx-auto mt-3 text-center text-white opacity-85 font-[550] text-[46px] font-montserrat tracking-[-.8px] leading-[30px] drop-shadow'>Desarrollo web<br/><span className='opacity-70 text-[32px] font-[600]'>profesional</span></h1>
