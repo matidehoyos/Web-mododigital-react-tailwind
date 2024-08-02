8d1bc6df573f46e7fb574ada1ffde991a198d65c
Matias <mati.dehoyosmdp@gmail.com>
2024-08-01 23:36:36 -0300

    bg

--- cliente/src/components/Header.jsx
+++ cliente/src/components/Header.jsx
@@ -2,9 +2,8 @@ import React from 'react';
 
 const Header = () => {
   return (
-        <div className="bg-[url('/bg.jpeg')] bg-cover bg-bottom h-[100vh] w-screen">
-                <div className='w-screen h-[100vh] bg-black opacity-60 absolute'></div>
-                <p className='m-auto pt-[90px] tracking-[0.3px] text-white opacity-80 text-center text-[18px] font-montserrat font-[600] italic'>¿Buscas una web a tu medida?</p>
+        <div className="bg-[url('/bgg.jpeg')] bg-cover h-[100vh] w-screen">
+                <p className='m-auto pt-[90px] tracking-[0.3px] text-gray-300 text-center text-[18px] font-montserrat font-[400] italic'>¿Buscas una web a tu medida?</p>
                 <img src="./marca1.png" alt="Logo Modo Digital." className='w-[72%] mx-auto mt-12 opacity-65' />
                 <h1 className='mx-auto text-center text-white font-[600] text-[52px] font-montserrat tracking-[-.8px] leading-[40px] drop-shadow opacity-80'>Diseño web<br/><span className='opacity-60'>profesional</span></h1>
                 <p className='w-[90%] mt-28 mx-auto p-2 text-pretty text-center text-white opacity-80 drop-shadow text-[22px] font-semibold leading-[24px]'>Prestigio, jerarquia y credibilidad para tu negocio.</p>
--- cliente/src/components/Tipos.jsx
+++ cliente/src/components/Tipos.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
-import { FaCheck } from 'react-icons/fa'
+import { FaCaretRight, FaCartPlus, FaCheck, FaStore } from 'react-icons/fa';
 
 const Tipos = () => {
   return (
     <div class="w-screen py-[50px] bg-gradient-to-tl from-white to-gray-200">
         <h2 className='font-bold text-center text-[20px] tracking-wide text-gray-800 drop-shadow'>¿Que tipos de webs creamos?</h2>
         <div className='w-screen flex-col pt-[40px]'>
-            <div className='w-[84%] mx-auto mb-6 flex justify-start items-center rounded-sm bg-gray-200'>
-                <img src="./ecommerce.jpeg" className='w-[42%] grayscale-[40%]' alt="Ilustracion de comercio electronico." />
-                <div className='pl-3'>
-                  <h3 className='text-[20px] text-gray-800 drop-shadow font-bold'>E-commerce</h3>
-                  <p className='text-[16px] text-gray-800 font-bold tracking-tight leading-[20px] opacity-80'>Vende por vos las 24hs.</p>
-                </div>
-            </div>
+            <div className='w-[84%] mx-auto mb-6 p-4 flex-col justify-start items-center rounded-sm bg-[#111] bg-opacity-85 rounded-[4px] border border-verde-900'>
+                  <FaStore className='text-verde-900 text-[20px] opacity-70' />
+                  <h3 className='text-[24px] mt-2 text-white drop-shadow font-bold'>Tienda online</h3>
+                  <ul class="mt-2 pt-2 list-none text-[16px] font-semibold drop-shadow text-white tracking-[.3px] opacity-70 border-t border-white border-opacity-10">
+                      <li class="flex items-baseline"><FaCheck class="mr-2 text-verde-900 opacity-80 text-[12px]" />Abierto las 24hs.</li>
+                      <li class="flex items-baseline"><FaCheck class="mr-2 text-verde-900 opacity-80 text-[12px]" />Carrito de compras.</li>
+                      <li class="flex items-baseline"><FaCheck class="mr-2 text-verde-900 opacity-80 text-[12px]" />Metodos de pago en linea.</li>
+                      <li class="flex items-baseline"><FaCheck class="mr-2 text-verde-900 opacity-80 text-[12px]"/>Productos ilimitados.</li>
+                      <li class="flex items-baseline"><FaCheck class="mr-2 text-verde-900 opacity-80 text-[12px]" />Autoadministrable.</li>
+                    </ul>            
+              </div>
             <div className='w-[84%] mx-auto mb-6 flex justify-start items-center rounded-sm bg-gray-200'>
                 <img src="./insti.jpeg" className='w-[42%] grayscale-[40%]' alt="Ilustracion de comercio electronico." />
                 <div className='pl-3'>
