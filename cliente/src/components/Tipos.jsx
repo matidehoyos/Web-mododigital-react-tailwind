2c4c13b66ad4087c67b9062b41e8be69b7b8ff29
Matias <mati.dehoyosmdp@gmail.com>
2024-08-02 13:47:25 -0300

    dsad

--- cliente/src/components/Tipos.jsx
+++ cliente/src/components/Tipos.jsx
@@ -3,10 +3,10 @@ import { FaBuilding, FaCaretRight, FaCartPlus, FaCheck, FaPlaneArrival, FaStore,
 
 const Tipos = () => {
   return (
-    <div class="w-screen pt-[50px] bg-[rgb(90,90,90)]">
-        <h2 className='font-bold text-center text-[20px] tracking-wide text-[#10d6e4] drop-shadow'>¿Que tipos de webs creamos?</h2>
+    <div class="w-screen pt-[50px] bg-[rgb(40,40,40)]">
+        <h2 className='font-bold text-center text-[20px] tracking-wide text-[#ffffffa2] drop-shadow'>¿Que tipos de webs creamos?</h2>
         <div className='w-screen flex-col pt-[40px]'>
-            <div className='w-[80%] mx-auto mb-8 p-4 flex-col justify-start items-center bg-[#272727] rounded-[12px] border border-[#07676e] shadow-lg shadow-[#07676e]'>
+            <div className='w-[80%] mx-auto mb-8 p-4 flex-col justify-start items-center bg-[#171717] rounded-[12px] border border-[#07676e] shadow-xl shadow-[#07676e7b]'>
                   <FaStore className='text-[#07676e] text-[20px]' />
                   <h3 className='text-[22px] mt-2 text-white opacity-70 drop-shadow font-bold'>Tienda online</h3>
                   <ul class="mt-2 pt-2 list-none text-[16px] font-semibold drop-shadow text-white tracking-[.3px] opacity-60 border-t border-[#07676e] border-opacity-50">
@@ -17,7 +17,7 @@ const Tipos = () => {
                       <li class="flex items-baseline"><FaCheck class="mr-2 text-[#07676e] text-[12px]" />Autoadministrable.</li>
                     </ul>            
               </div>
-            <div className='w-[80%] mx-auto mb-8 p-4 flex-col justify-start items-center bg-[#272727] rounded-[12px] border border-[#07676e] shadow-lg shadow-[#07676e]'>
+            <div className='w-[80%] mx-auto mb-8 p-4 flex-col justify-start items-center bg-[#171717] rounded-[12px] border border-[#07676e] shadow-xl shadow-[#07676e6b]'>
                   <FaBuilding className='text-[#07676e] text-[20px]' />
                   <h3 className='text-[22px] mt-2 text-white opacity-70 drop-shadow font-bold'>Institucional</h3>
                   <ul class="mt-2 pt-2 list-none text-[16px] font-semibold drop-shadow text-white tracking-[.3px] opacity-60 border-t border-[#07676e] border-opacity-50">
@@ -28,7 +28,7 @@ const Tipos = () => {
                       <li class="flex items-baseline"><FaCheck class="mr-2 text-[#07676e] text-[12px]" />Newsletter.</li>
                     </ul>            
               </div>
-            <div className='w-[80%] mx-auto mb-8 p-4 flex-col justify-start items-center bg-[#272727] rounded-[12px] border border-[#07676e] shadow-lg shadow-[#07676e]'>
+            <div className='w-[80%] mx-auto mb-8 p-4 flex-col justify-start items-center bg-[#171717] rounded-[12px] border border-[#07676e] shadow-xl shadow-[#07676e6c]'>
                   <FaPlaneArrival className='text-[#07676e] text-[20px]' />
                   <h3 className='text-[22px] mt-2 text-white opacity-70 drop-shadow font-bold'>Landing page</h3>
                   <ul class="mt-2 pt-2 list-none text-[16px] font-semibold drop-shadow text-white tracking-[.3px] opacity-60 border-t border-[#07676e] border-opacity-50">
@@ -38,7 +38,9 @@ const Tipos = () => {
                       <li class="flex items-baseline"><FaCheck class="mr-2 text-[#07676e] text-[12px]" />Enbudo de venta.</li>
                     </ul>            
               </div>
-              <h3 className='w-screen h-auto mt-[70px] py-[150px] bg-[url(diseno.jpeg)] bg-cover bg-bottom text-[28px] font-[700] text-center leading-[32px] tracking-[.4px] drop-shadow'>Diseños interactivos, profesionales y elegantes</h3>
+              <div className="w-screen mt-[70px] bg-[url('/diseno.jpeg')] bg-cover bg-center ">
+                  <h3 className='py-[150px] text-white text-[28px] font-[700] text-center leading-[32px] tracking-[.4px] drop-shadow'>Diseños interactivos, profesionales y elegantes</h3>
+              </div>
         </div> 
     </div>
   );
