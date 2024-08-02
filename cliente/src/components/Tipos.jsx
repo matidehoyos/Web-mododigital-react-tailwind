f65132f4af5809331630672d3fde9d2c48c2e61f
Matias <mati.dehoyosmdp@gmail.com>
2024-08-02 13:08:11 -0300

    fs

--- cliente/src/components/Tipos.jsx
+++ cliente/src/components/Tipos.jsx
@@ -3,10 +3,10 @@ import { FaBuilding, FaCaretRight, FaCartPlus, FaCheck, FaPlaneArrival, FaStore,
 
 const Tipos = () => {
   return (
-    <div class="w-screen py-[50px] bg-[rgb(90,90,90)]">
+    <div class="w-screen pt-[50px] bg-[rgb(90,90,90)]">
         <h2 className='font-bold text-center text-[20px] tracking-wide text-[#10d6e4] drop-shadow'>¿Que tipos de webs creamos?</h2>
         <div className='w-screen flex-col pt-[40px]'>
-            <div className='w-[80%] mx-auto mb-8 p-4 flex-col justify-start items-center bg-[#272727] rounded-[12px] border border-[#033b3f] shadow-lg shadow-[#033b3f]'>
+            <div className='w-[80%] mx-auto mb-8 p-4 flex-col justify-start items-center bg-[#272727] rounded-[12px] border border-[#07676e] shadow-lg shadow-[#07676e]'>
                   <FaStore className='text-[#07676e] text-[20px]' />
                   <h3 className='text-[22px] mt-2 text-white opacity-70 drop-shadow font-bold'>Tienda online</h3>
                   <ul class="mt-2 pt-2 list-none text-[16px] font-semibold drop-shadow text-white tracking-[.3px] opacity-60 border-t border-[#07676e] border-opacity-50">
@@ -38,13 +38,7 @@ const Tipos = () => {
                       <li class="flex items-baseline"><FaCheck class="mr-2 text-[#07676e] text-[12px]" />Enbudo de venta.</li>
                     </ul>            
               </div>
-           
-           
-         {/*}   <div className='w-[84%] mx-auto py-[40px] rounded-sm bg-gray-300'>
-                <h3 className='text-[24px] tracking-wide leading-[40px] font-[700] text-center text-gray-800 opacity-75 drop-shadow border border-gray-900'>BLOGS</h3>
-                <h3 className='text-[24px] tracking-wide leading-[40px] font-[700] text-center  text-gray-800 opacity-75 drop-shadow border border-gray-900'>PORTFOLIOS</h3>
-                <h3 className='text-[24px] tracking-wide leading-[40px] font-[700] text-center text-gray-800 opacity-75 drop-shadow border border-gray-900'>SOCIAL WEBS</h3>
-            </div>*/ }
+              <h3 className='w-screen h-auto mt-[70px] py-[150px] bg-[url(diseno.jpeg)] bg-cover bg-bottom text-[28px] font-[700] text-center leading-[32px] tracking-[.4px] drop-shadow'>Diseños interactivos, profesionales y elegantes</h3>
         </div> 
     </div>
   );
