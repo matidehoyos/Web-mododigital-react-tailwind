92d7f520e06a4e5000bb9bf9a39d80fae05d37ff
Matias <mati.dehoyosmdp@gmail.com>
2024-08-03 18:10:30 -0300

    img

--- cliente/src/components/Contacto.jsx
+++ cliente/src/components/Contacto.jsx
@@ -1,6 +1,7 @@
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { useEffect } from 'react';
+import { FaPhone } from 'react-icons/fa';
 
 
 
@@ -11,11 +12,10 @@ const Contacto = () => {
 
   return (
     <div class="w-screen py-[70px] md:py-[120px] px-[20px] overflow-x-hidden bg-[rgb(245,245,245)]" >
-          <p className='text-black text-[22px] md:text-[32px] text-center font-[700] md:font-[700] tracking-[0px] leading-[22px] md:leading-[36px]' data-aos="fade-up" data-aos-duration="800">Contáctanos y expandi tu negocio</p>
           <p className='mt-[10px] text-black opacity-90 text-[20px] md:text-[32px] text-center font-[700] tracking-[0px] leading-[22px] md:leading-[36px]' data-aos="fade-up" data-aos-duration="800">Contanos sobre tu proyecto</p>
           <p className='mt-[10px] text-black opacity-85 text-[20px] md:text-[32px] text-center font-[700] tracking-[0px] leading-[22px] md:leading-[36px]' data-aos="fade-up" data-aos-duration="800">Cotizá tu sitio ya!</p>
           <div className='flex justify-center'>
-                  <a href="https://api.whatsapp.com/send?phone=542236901801" target='blank' className="botom mt-[32px] md:mt-16 py-[6px] md:py-[8px] px-[12px] md:px-[16px] text-azul-900 text-[16px] md:text-[20px] font-[600] tracking-[.3px] drop-shadow border border-azul-900 rounded-[4px] shadow-xl shadow-[#45a4ad72] xl:hover:bg-azul-900 xl:hover:text-white" data-aos="fade-up" data-aos-duration="800">
+                  <a href="https://api.whatsapp.com/send?phone=542236901801" target='blank' className="botom mt-[32px] md:mt-10 py-[6px] md:py-[8px] px-[12px] md:px-[16px] text-azul-900 text-[16px] md:text-[20px] font-[600] tracking-[.3px] drop-shadow border border-azul-900 rounded-[4px] shadow-xl shadow-[#45a4ad72] xl:hover:bg-azul-900 xl:hover:text-white" data-aos="fade-up" data-aos-duration="800">
                     Contáctanos
                   </a>
            </div>
