0053c36626997acc13555d1914274e45fbd078a8
Matias <mati.dehoyosmdp@gmail.com>
2024-08-02 21:09:01 -0300

    }

--- cliente/src/components/Caracterist.jsx
+++ cliente/src/components/Caracterist.jsx
@@ -22,13 +22,13 @@ const Carasterist = () => {
   
 
   return (
-    <div className="w-screen py-[40px] flex-col items-center justify-center bg-[rgb(220,220,220)]">
+    <div className="w-screen px-[25px] pb-[40px] flex-col items-center justify-center bg-[rgb(220,220,220)]">
         {
             data.map(box => (
-                <div className="w-[100%] mb-[45px] px-8">
+                <div className="w-[100%] mb-[45px]">
                     <h2 className="text-xl text-gray-800 font-semibold mb-2">{box.title}</h2>
                     <img src={box.image} alt={box.title} className="w-full h-[140px] object-cover object-top mb-2 opacity-80"  />
-                    <p className='text-gray-900 text-[17px] font-[500] tracking-[.5px] leading-[21px]'>{box.paragraph}</p>
+                    <p className='text-gray-700 text-[17px] font-[650] tracking-[.5px] leading-[21px]'>{box.paragraph}</p>
                 </div>
             ))
         }
