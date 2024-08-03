47b72834f6108c02fd3cdcb319cc32f6c36bfad4
Matias <mati.dehoyosmdp@gmail.com>
2024-08-02 22:50:38 -0300

    fs

--- cliente/src/components/Portfolio.jsx
+++ cliente/src/components/Portfolio.jsx
@@ -5,7 +5,7 @@ const Portfolio = () => {
     <div class="w-screen py-[70px] px-[15px] overflow-x-hidden bg-[#1c3133]">
           <h4 className=' mb-[12px] text-[20px] text-[#b9b9b9] tracking-wide leading-[21px] font-[600]'>PORTFOLIOS<br/><span className='text-[#548589] text-[17px] leading-none'>para profesionales</span></h4>
           <img src="porfolio1.jpeg" alt="imagen website" className='w-[75%] h-[140px] object-cover object-top opacity-80 rounded-md' />
-          <p className='w-[98%] mt-[12px] text-white opacity-85 text-[18px] font-[300] leading-tight tracking-[.8px] '>Una web que muestre a la perfeccion tus proyectos, tu carrera, tu preparacion, y todo lo que se refiera a tu profesion. <br/> </p>
+          <p className='w-[98%] mt-[12px] text-white opacity-85 text-[16px] font-[600] leading-tight tracking-[.86px] '>Una web que muestre a la perfeccion tus proyectos, tu carrera, tu preparacion, y todo lo que se refiera a tu profesion. <br/> </p>
     </div>
   );
 };
