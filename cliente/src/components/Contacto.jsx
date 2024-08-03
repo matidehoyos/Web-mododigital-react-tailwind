db3aa4ce5aee1663d71aa2858dcb1c240088e449
Matias <mati.dehoyosmdp@gmail.com>
2024-08-03 17:35:42 -0300

    ¨fs¨

--- cliente/src/components/Contacto.jsx
+++ cliente/src/components/Contacto.jsx
@@ -11,7 +11,7 @@ const Contacto = () => {
 
   return (
     <div class="w-screen py-[70px] md:py-[120px] px-[20px] overflow-x-hidden bg-[rgb(245,245,245)]" >
-          <p className='text-black text-[22px] md:text-[32px] text-center font-[700] md:font-[700] tracking-[0px] leading-[22px] md:leading-[36px]' data-aos="fade-up" data-aos-duration="800">Hagamos algo increible juntos</p>
+          <p className='text-black text-[22px] md:text-[32px] text-center font-[700] md:font-[700] tracking-[0px] leading-[22px] md:leading-[36px]' data-aos="fade-up" data-aos-duration="800">Contáctanos y expandi tu negocio</p>
           <p className='mt-[10px] text-black opacity-90 text-[20px] md:text-[32px] text-center font-[700] tracking-[0px] leading-[22px] md:leading-[36px]' data-aos="fade-up" data-aos-duration="800">Contanos sobre tu proyecto</p>
           <p className='mt-[10px] text-black opacity-85 text-[20px] md:text-[32px] text-center font-[700] tracking-[0px] leading-[22px] md:leading-[36px]' data-aos="fade-up" data-aos-duration="800">Cotizá tu sitio ya!</p>
           <div className='flex justify-center'>
