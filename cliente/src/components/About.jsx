e8c1569d7f84c33789661b924ea357231d6aaded
Matias <mati.dehoyosmdp@gmail.com>
2024-08-03 11:04:16 -0300

    ddgi

--- cliente/src/components/About.jsx
+++ cliente/src/components/About.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div class="w-screen py-[70px] xl:py-[100px] px-[20px] overflow-x-hidden bg-[#111]">
-       <img src="md.png" className='w-[50%] opacity-90' alt="Logo modo digital" />
-       <p className='mt-[22px] text-[18px] font-[500] text-gray-500 tracking-[.3px] leading-[24px] italic'>Somos una agencia de desarrollo web, con base en Mar Del Plata, Argentina. Nos apaciona la innovacion y el descubrimiento de nuevas tecnologias. Nos actualizamos constantemente, para siempre garantizar un producto vigente, innovador, y vanguardia.</p>
+    <div class="w-screen py-[70px] xl:py-[120px] px-[20px] xl:px-[150px] overflow-x-hidden bg-[#111]">
+       <img src="md.png" className='w-[50%] xl:w-[34%] xl:mx-auto opacity-90' alt="Logo modo digital" />
+       <p className='xl:w-[70%] mt-[22px] xl:mx-auto xl:mt-[30px] text-[18px] xl:text-[24px] font-[500] xl:text-center text-gray-500 tracking-[.3px] leading-[24px] xl:leading-[30px] italic'>Somos una agencia de desarrollo web, con base en Mar Del Plata, Argentina. Nos apaciona la innovacion y el descubrimiento de nuevas tecnologias. Nos actualizamos constantemente, para siempre garantizar un producto vigente, innovador, y vanguardia.</p>
     </div>
   );
 };
--- cliente/src/components/Contacto.jsx
+++ cliente/src/components/Contacto.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 const Contacto = () => {
   return (
-    <div class="w-screen py-[70px] xl:py-[100px] px-[20px] overflow-x-hidden bg-[rgb(245,245,245)] xl:bg-[#111]">
+    <div class="w-screen py-[70px] xl:py-[120px] px-[20px] overflow-x-hidden bg-[rgb(245,245,245)] ">
           <p className='text-black text-[22px] xl:text-[32px] text-center font-[700] xl:font-[700] tracking-[0px] leading-[22px] xl:leading-[36px]'>Hagamos algo increible juntos</p>
           <p className='mt-[10px] text-black opacity-90 text-[20px] xl:text-[32px] text-center font-[700] tracking-[0px] leading-[22px] xl:leading-[36px] '>Contanos sobre tu proyecto</p>
           <p className='mt-[10px] text-black opacity-80 text-[18px] xl:text-[32px] text-center font-[700] tracking-[0px] leading-[22px] xl:leading-[36px] '>Cotiza tu sitio ya!</p>
           <div className='flex justify-center'>
-                  <a href="https://api.whatsapp.com/send?phone=542236901801" target='blank' className="botom mt-[32px] xl:mt-6 py-[6px] xl:py-[10px] px-[12px] xl:px-[20px] text-azul-900 text-[14px] xl:text-[20px] font-[600] tracking-[.3px] drop-shadow border border-azul-900 rounded-[6px] shadow-xl shadow-[#45a5ad98]">
+                  <a href="https://api.whatsapp.com/send?phone=542236901801" target='blank' className="botom mt-[32px] xl:mt-16 py-[6px] xl:py-[8px] px-[12px] xl:px-[16px] text-azul-900 text-[14px] xl:text-[20px] font-[600] tracking-[.3px] drop-shadow border border-azul-900 rounded-[6px] shadow-xl shadow-[#45a5ad98] xl:hover:bg-azul-900 xl:hover:text-white">
                     Contáctanos
                   </a>
            </div>
