63a8b2e37e1a5edce7ee6443239e126989c16f0f
Matias <mati.dehoyosmdp@gmail.com>
2024-08-03 19:19:43 -0300

    d

--- cliente/src/components/Footer.jsx
+++ cliente/src/components/Footer.jsx
@@ -45,9 +45,9 @@ const Footer = () => {
     <div className='bg-black pt-[70px] xl:pt-[120px]'>
           <div className='mb-[40px]'>
             <p className="w-[100%] text-[18px] xl:text-[22px] font-[500] text-gray-500 text-center"><IoLocationSharp className='inline text-azul-900 text-[22px] xl:text-[26px] opacity-80 mr-1' /> Mar Del Plata - Arg</p>
-            <p className="w-[100%] mt-3 text-[18px] xl:text-[22px] font-[500] text-gray-500 text-center"><a href="mailto:mati.dehoyosmdp@gmail.com" target='_blank' ><FaEnvelopeSquare className='inline text-azul-900 text-[22px] xl:text-[26px] opacity-70 mr-2'/>mododigital@gmail.com</a></p>
+            <p className="w-[100%] mt-3 text-[18px] xl:text-[22px] font-[500] text-gray-500 text-center"><a href="mailto:mati.dehoyosmdp@gmail.com" target='_blank' ><FaEnvelopeSquare className='inline text-azul-900 text-[22px] xl:text-[26px] opacity-70 mr-2'/>contacto@mododigital.com</a></p>
             <p className="w-[100%] mt-3 text-[18px] xl:text-[22px] font-[500] text-gray-500 text-center"><a href="https://api.whatsapp.com/send?phone=542236901801" target='_blank'><FaWhatsapp  className='inline text-azul-900 text-[22px] xl:text-[26px] opacity-80 mr-1' />+54 9 2236901801</a></p>
-            <p className="w-[100%] mt-3 text-[18px] xl:text-[22px] font-[500] text-gray-500 text-center"><a target="_blank" href='https://www.instagram.com/mododigital.tech/'><FaInstagram className="inline text-azul-900 text-[22px] xl:text-[26px] opacity-80 mr-2"/>mododigital.tech</a></p>
+            <p className="w-[100%] mt-3 text-[18px] xl:text-[22px] font-[500] text-gray-500 text-center"><a target="_blank" href='https://www.instagram.com/mododigital.tech/'><FaInstagram className="inline text-azul-900 text-[22px] xl:text-[26px] opacity-80 mr-2"/>instagram</a></p>
           </div>
 
          <div className="w-screen my-[60px] flex justify-center"><a className='px-5 py-3 bg-[#111] border border-[#222] rounded-md' href="#" onClick={() => window.scrollTo(0, 0)}><FaArrowUp className="text-gray-400 text-[24px] font-[800]"/></a></div>
