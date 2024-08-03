a4546e20ed3543fefa727525ec81c946598724d6
Matias <mati.dehoyosmdp@gmail.com>
2024-08-03 13:05:25 -0300

    sd

--- cliente/src/components/Header.jsx
+++ cliente/src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
 
 
   return (
-        <div className="bg-[url('/bg8.jpeg')] bg-cover bg-center bg-no-repeat xl:bg-[url('/bgxl.jpeg')] xl:bg-bottom xl:bg-fixed h-[100vh] xl:h-[900px] xl:pb-[120px] w-screen">
+        <div className="bg-[url('/bg7.jpg')] bg-cover bg-center bg-no-repeat xl:bg-[url('/bgxl.jpeg')] xl:bg-bottom xl:bg-fixed h-[100vh] xl:h-[900px] xl:pb-[120px] w-screen">
                 <div className='w-screen h-[100vh] xl:h-[900px] bg-black bg-opacity-70 xl:bg-opacity-65 blur-sm xl:blur-lg absolute overflow-hidden'></div>
                 <button className='p-[9px] xl:p-[16px] z-50 bg-[#000] fixed bottom-[15px] right-[10px] xl:right-[20px] rounded-[50%] border border-verde-900'><a href="https://api.whatsapp.com/send?phone=542236901801" target="_blank" rel="noopener noreferrer"><FaWhatsapp className='text-[34px] text-verde-900'/></a></button>
                 <p className='m-auto pt-[80px] xl:pt-[120px] tracking-[0.3px] text-[rgb(180,180,180)] opacity-95 text-center text-[16px] xl:text-[24px] font-montserrat font-[600] italic'>¿Buscas un sitio a tu medida?</p>
