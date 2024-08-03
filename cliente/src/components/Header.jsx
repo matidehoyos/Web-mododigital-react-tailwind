dc604b496e832a5aa1eaba349bfa075acc5b0998
Matias <mati.dehoyosmdp@gmail.com>
2024-08-03 17:50:50 -0300

    ¨ee¨

--- cliente/src/components/Header.jsx
+++ cliente/src/components/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
                 <h1 className='mx-auto mt-2 md:mt-4 xl:mt-5 text-center text-white opacity-75 font-[550] text-[42px] md:text-[56px] xl:text-[72px] font-montserrat tracking-[-.8px] leading-[28px] drop-shadow' data-aos="fade-up" data-aos-duration="800">Desarrollo web<br/><span className='opacity-55 text-[32px] md:text-[36px] md:relative md:top-3 xl:relative xl:top-5 xl:text-[42px] font-[600]'>profesional</span></h1>
                 <p className='w-[92%] mt-20 md:mt-[140px] xl:mt-[140px] mx-auto p-2 text-pretty text-center text-[rgb(180,180,180)] opacity-95 text-[18px] md:text-[22px] xl:text-[23px] font-[600] tracking-[.3px] leading-[25px] font-montserrat italic' data-aos="fade-up" data-aos-duration="800">Prestigio, jerarquía y credibilidad para tu negocio.</p>
                 <div className="flex justify-center">
-                  <a href="#tipos" className="botom mt-14 py-[6px] px-[14px] text-azul-900 text-[16px] md:text-[18px] font-[600] tracking-[.3px] drop-shadow border border-azul-900 rounded-[6px]" data-aos="fade-up" data-aos-duration="800" >
+                  <a href="#tipos" className="botom mt-12 py-[6px] px-[14px] text-azul-900 text-[16px] md:text-[18px] font-[600] tracking-[.3px] drop-shadow border border-azul-900 rounded-[6px]" data-aos="fade-up" data-aos-duration="800" >
                     Comencemos
                   </a>
                 </div>
