3fe813c2a29b83af26149a3c2115d3fe89c6c139
Matias <mati.dehoyosmdp@gmail.com>
2024-08-04 12:21:21 -0300

    ¨s¨

--- cliente/src/components/Footer.jsx
+++ cliente/src/components/Footer.jsx
@@ -45,7 +45,7 @@ const Footer = () => {
     <div className='bg-black pt-[70px] xl:pt-[120px]'>
           <div className='mb-[40px]'>
             <p className="w-[100%] text-[18px] xl:text-[22px] font-[500] text-gray-500 text-center"><IoLocationSharp className='inline text-azul-900 text-[22px] xl:text-[26px] opacity-80 mr-1' /> Mar Del Plata - Arg</p>
-            <p className="w-[100%] mt-3 text-[18px] xl:text-[22px] font-[500] text-gray-500 text-center"><a href="mailto:mati.dehoyosmdp@gmail.com" target='_blank' ><FaEnvelopeSquare className='inline text-azul-900 text-[22px] xl:text-[26px] opacity-70 mr-2'/>contacto@mododigital.com</a></p>
+            <p className="w-[100%] mt-3 text-[18px] xl:text-[22px] font-[500] text-gray-500 text-center"><a href="mailto:mati.dehoyosmdp@gmail.com" target='_blank' ><FaEnvelope className='inline text-azul-900 text-[22px] xl:text-[26px] opacity-70 mr-2'/>contacto@mododigital.com</a></p>
             <p className="w-[100%] mt-3 text-[18px] xl:text-[22px] font-[500] text-gray-500 text-center"><a href="https://api.whatsapp.com/send?phone=542236901801" target='_blank'><FaWhatsapp  className='inline text-azul-900 text-[22px] xl:text-[26px] opacity-80 mr-1' />+54 9 2236901801</a></p>
             <p className="w-[100%] mt-3 text-[18px] xl:text-[22px] font-[500] text-gray-500 text-center"><a target="_blank" href='https://www.instagram.com/mododigital.tech/'><FaInstagram className="inline text-azul-900 text-[22px] xl:text-[26px] opacity-80 mr-2"/>instagram</a></p>
           </div>
