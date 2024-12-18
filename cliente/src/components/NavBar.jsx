9c0caf933e55b3a581a5093ea0a622f4df97471c
Matias <mati.dehoyosmdp@gmail.com>
2024-12-18 12:59:43 +0000

    new design

--- cliente/src/components/NavBar.jsx
+++ cliente/src/components/NavBar.jsx
@@ -33,10 +33,10 @@ const NavBar = () => {
           <li><a href="#contacto" className='text-lg text-green-500 lg:hover:text-azul-900'>Contacto</a></li>
           <li><a href="#frec" className='text-lg text-green-500 lg:hover:text-azul-900'>Faq</a></li>
         </ul>
-        <ul className={`w-full flex flex-col items-center space-y-3 md:space-x-5 mr-[10px] absolute ${isVisible ? 'top-[48px] h-[60vh] py-14 bg-[#033b3f]' : '-top-[1000] h-0 py-0 overflow-hidden bg-transparent'} transition-all duration-1000 lg:hidden z-40`}>
-          <li><a href="#tipos" className='text-[24px] font-bold text-center text-green-500'>Servicios</a></li>
-          <li><a href="#contacto" className='text-[24px] font-bold text-center text-green-500'>Contacto</a></li>
-          <li><a href="#frec" className='text-[24px] font-bold text-center text-green-500'>Faq</a></li>
+        <ul className={`w-full flex flex-col items-center space-y-3 md:space-x-5 mr-[10px] absolute ${isVisible ? 'top-[50px] h-[calc(100vh-50px)] py-14 bg-[#033b3f]' : '-top-[1000] h-0 py-0 overflow-hidden bg-transparent'} transition-all duration-1000 lg:hidden z-40`}>
+          <li><button onClick={handleShowMenu}><a href="#tipos" className='text-[24px] font-bold text-center text-green-500'>Servicios</a></button></li>
+          <li><button onClick={handleShowMenu}><a href="#contacto" className='text-[24px] font-bold text-center text-green-500'>Contacto</a></button></li>
+          <li><button onClick={handleShowMenu}><a href="#frec" className='text-[24px] font-bold text-center text-green-500'>Faq</a></button></li>
         </ul>
         <button className='text-green-500 mr-[10px] lg:hidden' onClick={handleShowMenu}>
             {isVisible ? <HiX size={32}/> : <HiMenu size={32}/>}
